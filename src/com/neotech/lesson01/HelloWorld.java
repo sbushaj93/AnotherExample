040453aabfee439060a0f4efe4929d0bb8815804
muskaj <muskaj2021@gmail.com>
2025-01-16 18:48:08 -0400

    added a comment

--- src/com/neotech/lesson01/HelloWorld.java
+++ src/com/neotech/lesson01/HelloWorld.java
@@ -3,6 +3,9 @@ package com.neotech.lesson01;
 public class HelloWorld { // this marks the beginning of the class
 
 	// to save changes: CTRL + S
+	
+	
+	//just added another comment
 
 	// the main method is the starting point to run a java application
 	public static void main(String[] args) { // this is the beginning of the method
